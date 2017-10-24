299f4b365cde1de0907184cfcc2ab26e1ac03ddf
Rama <ramkrishnadevops25@gmsil.com>
2017-10-23 22:41:07 -0400

    my second change.Added else part

--- myfirstvcs.py
+++ myfirstvcs.py
@@ -3,4 +3,7 @@ print "This is my first program"
 a=str(raw_input(Enter your name))
 if a == 'Rama':
     print "Welcome to git"
+
+else:
+    print" Welcome a even though if you rae not a ram"
         
